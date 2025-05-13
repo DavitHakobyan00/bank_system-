08fd7e52065f34281690215d87dd14cf151784e6
Davit_Hakobyan <davithakobyan007@gmail.com>
2025-05-13 11:21:20 +0400

    system

---
+++ src/model/CardAccount.java
@@ -0,0 +1,69 @@
+package model;
+import model.enums.*;
+public class CardAccount {
+        private String accountNumber;
+        private String cvv;
+        private String expirationDate;
+        private CardType cardType;
+        private double balance;
+        private CardStatus cardStatus;
+        private Currency currency;
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getCvv() {
+        return cvv;
+    }
+
+    public void setCvv(String cvv) {
+        this.cvv = cvv;
+    }
+
+    public String getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(String expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+    public CardType getCardType() {
+        return cardType;
+    }
+
+    public void setCardType(CardType cardType) {
+        this.cardType = cardType;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public CardStatus getCardStatus() {
+        return cardStatus;
+    }
+
+    public void setCardStatus(CardStatus cardStatus) {
+        this.cardStatus = cardStatus;
+    }
+
+    public Currency getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(Currency currency) {
+        this.currency = currency;
+    }
+}
+
+
---
+++ src/model/CardHolder.java
@@ -0,0 +1,11 @@
+package model;
+public class CardHolder {
+        private String passportNumber;
+        private String fullName;
+        private Address address;
+        ?card accaut list
+        private BankAccount bankAccount;
+ }
+
+
+
